6b0f5a1840ff914555bcaf2265b0d71122e69d6c
oleksa-inspirators <oleksandr@inspirators.eu>
2024-10-13 18:44:56 +0300

    background map

---
+++ src/Map.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { onMount } from 'svelte';
+    import 'leaflet/dist/leaflet.css';
+    import L from 'leaflet';
+  
+    let map;
+  
+    onMount(() => {
+      // Ініціалізація мапи
+      map = L.map('map', {
+        center: [49.8419, 24.0315], // Центр Львова
+        zoom: 7.3,
+        dragging: false, // Заборона панорамування
+        scrollWheelZoom: false, // Заборона зуму
+        doubleClickZoom: false, // Заборона зуму при двократному натисканні
+        boxZoom: false, // Заборона зуму за допомогою коробки
+      });
+  
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+      }).addTo(map);
+    });
+  </script>
+  
+  <style>
+    #map {
+      width: 100%;
+      height: 100vh; 
+    }
+  </style>
+  
+  <div id="map"></div>
+  
